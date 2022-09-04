1e5f28aedf1212e09c4883c8893e51684a079f27
Carlos Cerquera Losada <ccerquer@yahoo.com>
2022-09-04 00:12:07 -0500

    Clone for GitHub

---
+++ GymEnCasa.App.Presentacion/Pages/Usuarios/IniciarSesion.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GymEnCasa.App.Presentacion.Pages
+{
+    public class IniciarSesionModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
